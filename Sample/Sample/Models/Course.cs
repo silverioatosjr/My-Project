632bfab9ae00955f70daecfb093dc655a1623476
Silverio Atos Jr <silverioatosjr@gmail.com>
2016-01-12 21:30:36 +0800

    sample2

---
+++ Sample/Sample/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample.Models
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string Description { get; set; }
+    }
+}
